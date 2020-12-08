248e5e6989ad6f2e6d6cfb59cb2f3fb41ebb6a80
Iwanek <pati.iwanek@gmail.com>
2020-12-08 20:13:17 +0100

    challenge3

--- challenge3.py
+++ challenge3.py
@@ -1,4 +1,25 @@
 import os
 
-f = open(os.getcwd() + '/input_challenge2.txt', 'r')
+f = open(os.getcwd() + '/input3.txt', 'r')
 listWithVal = f.read().split('\n')
+
+# 3 in the right, 1 in the bottom
+startIndex = 0
+treesInTheSlope = 0
+sentencesMultiplication = 1
+for i in range(2, len(listWithVal), 2):
+    sentence = listWithVal[i]*1200
+    startIndex +=1
+    # print(startIndex)
+    # print(sentence[startIndex])
+    if sentence[startIndex] == "#":
+        treesInTheSlope+=1
+
+
+print(treesInTheSlope)
+
+# 191  dla 3 w prawo i 1 w dół
+# 60 dla 1 w prawo i 1 w dół
+# 63 dla 7 w prawo i 1 w dół
+# 64 dla 5 w prawo i 1 w dół
+# 32 dla 1 w prawo i 2 w dół
